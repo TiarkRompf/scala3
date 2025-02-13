0b39147b12c75c522eb43993a312200d6787d638
cliu369 <craig.cliu@gmail.com>
2025-02-13 14:16:48 -0500

    Added effect checking for applications

--- compiler/src/dotty/tools/dotc/eff/EffectTypes.scala
+++ compiler/src/dotty/tools/dotc/eff/EffectTypes.scala
@@ -4,36 +4,14 @@ package eff
 
 import core.*
 import Types.*, Symbols.*, Contexts.*, Annotations.*
-import printing.{Showable, Printer, Texts}
-import Texts.Text
 import ast.tpd
 
-enum BinaryEff extends Showable:
-  case Bot, IO
-
-  def join(that: BinaryEff): BinaryEff = {
-    that match
-      case Bot => this
-      case IO => that
-  }
-
-  def subsume(that: BinaryEff): Boolean = this.join(that) == that
-
-  def |>(that: BinaryEff): BinaryEff = {
-    (this, that) match
-      case (Bot, Bot) => Bot
-      case _ => IO
-  }
-
-  def toText(printer: Printer): Text =
-    this match
-      case Bot => "Bot"
-      case IO => "IO"
-
-
 case class EffectAnnotation() extends Annotation:
-  // temporary tree for now, later make it like CaptureAnnotation and have EffectAnnotation take in symbol
-  def tree(using Context): tpd.Tree = tpd.New(defn.AnnotationDefaultAnnot.typeRef, Nil) // temp for now
+  // temporary tree for now, later make it like CaptureAnnotation and have EffectAnnotation take in symbol?
+  def tree(using Context): tpd.Tree =
+    // println(ctx.phase)
+    tpd.Literal(Constants.Constant("eff"))
+    // tpd.New(defn.AnnotationDefaultAnnot.typeRef, Nil) // temp for now
 
   def |>(that: EffectAnnotation)(using Context): EffectAnnotation = // maybe useful in future?
     EffectAnnotation()
@@ -61,28 +39,6 @@ extension (tp: Type)
       case EffectType(_) => true
       case _ => false
   }
-
-//   def extract(using Context): Type = {
-//     tp match
-//       case EffectType(parent, _) => parent
-//       case _ => tp
-//   }
-
-//   def |>(that: Type)(using Context): BinaryEff = {
-//     (tp, that) match
-//       case (EffectType(_, tpEff), EffectType(_, thatEff)) => tpEff |> thatEff
-//       case (EffectType(_, tpEff), _) => tpEff
-//       case (_, EffectType(_, thatEff)) => thatEff
-//       case _ => BinaryEff.Bot
-//   }
-
-//   def >>=(op: Type => Type)(using Context): Type = {
-//     tp match
-//       case EffectType(parent, eff) => EffectType(op(parent), eff)
-//       case _ => op(tp)
-//   }
-
-
 
 
 
